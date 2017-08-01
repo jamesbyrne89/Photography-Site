0cba89ce28d04b9205b995ea7747d698e4dcd6c4
jamesbyrne89 <mail@jamestbyrne.com>
2017-08-01 02:54:10 +0100

    Fixed broken links and menu

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('watch', function(){
 
 // Watch CSS for changes and inject compiled and minified CSS
 
-watch('app/assets/styles/*.css', function(){
+watch('app/assets/styles/**/*.css', function(){
 gulp.start('cssInject');
 });
 
