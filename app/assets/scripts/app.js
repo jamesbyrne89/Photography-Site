65a5276d556af494f15c7168ef6131688bf1c195
James Byrne <mail@jamestbyrne.com>
2017-08-03 11:04:13 +0100

    Adjusted site logo and scripts

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,15 +2,22 @@ const mobileBtn = document.getElementById('js-mobile-menu-btn');
 const mobileCloseBtn = document.getElementById('js-menu-close-btn');
 const mobileDrawer = document.getElementById('js-mobile-menu-drawer');
 const darken = document.getElementById('js-darken');
+const container = document.getElementById('js-container');
 
+// Slide out menu on click
 mobileBtn.addEventListener('click', function(){
   mobileDrawer.classList.toggle('active');
-  darken.classList.toggle('hidden');
+  mobileDrawer.classList.toggle('menu--active');
+  darken.classList.remove('hidden');
+  container.classList.toggle('active');
 });
 
+// Close button on click
 mobileCloseBtn.addEventListener('click', function(){
   mobileDrawer.classList.toggle('active');
-  darken.classList.toggle('hidden');
+  mobileDrawer.classList.toggle('menu--active');
+  container.classList.toggle('active');
+  darken.classList.add('hidden');
 });
 
 // Close menu on click away
@@ -20,9 +27,12 @@ if (mobileDrawer.classList.contains('active') && e.target !== mobileBtn) {
     // if the target of the click isn't the container nor a descendant of the container
     if (mobileDrawer !== e.target && e.target.parentNode !== mobileDrawer) {
         mobileDrawer.classList.remove('active');
+        mobileDrawer.classList.remove('menu--active');
         darken.classList.add('hidden');
     }
   }  
 }, false);
 
+
+// Lightbox
 $('.photo-square').featherlight($content, configuration);
--- app/temp/assets/scripts/babel/app.js
+++ app/temp/assets/scripts/babel/app.js
@@ -4,15 +4,22 @@ var mobileBtn = document.getElementById('js-mobile-menu-btn');
 var mobileCloseBtn = document.getElementById('js-menu-close-btn');
 var mobileDrawer = document.getElementById('js-mobile-menu-drawer');
 var darken = document.getElementById('js-darken');
+var container = document.getElementById('js-container');
 
+// Slide out menu on click
 mobileBtn.addEventListener('click', function () {
   mobileDrawer.classList.toggle('active');
-  darken.classList.toggle('hidden');
+  mobileDrawer.classList.toggle('menu--active');
+  darken.classList.remove('hidden');
+  container.classList.toggle('active');
 });
 
+// Close button on click
 mobileCloseBtn.addEventListener('click', function () {
   mobileDrawer.classList.toggle('active');
-  darken.classList.toggle('hidden');
+  mobileDrawer.classList.toggle('menu--active');
+  container.classList.toggle('active');
+  darken.classList.add('hidden');
 });
 
 // Close menu on click away
@@ -22,9 +29,11 @@ document.addEventListener('click', function (e) {
     // if the target of the click isn't the container nor a descendant of the container
     if (mobileDrawer !== e.target && e.target.parentNode !== mobileDrawer) {
       mobileDrawer.classList.remove('active');
+      mobileDrawer.classList.remove('menu--active');
       darken.classList.add('hidden');
     }
   }
 }, false);
 
+// Lightbox
 $('.photo-square').featherlight($content, configuration);
